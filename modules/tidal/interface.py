1501025341fe356edba5a51dcc6f0343ef1e0f14
Myo Ko Aung <111551988+MKA2025@users.noreply.github.com>
2024-11-07 18:30:11 +0630

    Update interface.py

--- modules/tidal/interface.py
+++ modules/tidal/interface.py
@@ -1,168 +1,175 @@
-from typing import Optional, Dict, Any, List
-from utils.models import Track, Album, Playlist
-from utils.exceptions import AuthenticationError, InvalidURLError, DownloadError
-from .api import TidalApi
-from .utils import parse_tidal_url, format_track_filename, get_quality_string
+from typing import Optional, List
+from orpheus.core import *
+from .tidal_api import TidalApi
+from .exceptions import *
 
-class TidalInterface:
-    def __init__(self, config: Dict[str, Any]):
-        self.config = config
+class TidalInterface(ModuleInterface):
+    def __init__(self, module_controller: ModuleController):
+        super().__init__(module_controller)
         self.api = TidalApi(
-            tv_token=config.get('tv_token'),
-            tv_secret=config.get('tv_secret'),
-            mobile_token=config.get('mobile_atmos_token'),
-            enable_mobile=config.get('enable_mobile', True)
+            self.module_settings['tv_token'],
+            self.module_settings['tv_secret'],
+            self.module_settings['mobile_atmos_token'],
+            self.module_settings['mobile_default_token'],
+            self.module_settings['enable_mobile'],
+            TidalError
         )
+
+    def login(self, email: Optional[str] = None, password: Optional[str] = None):
+        try:
+            self.api.login()
+        except Exception as e:
+            raise ModuleError(str(e))
+
+    def get_track_info(self, track_id: str, quality_tier: QualityEnum, codec_options: CodecOptions) -> TrackInfo:
+        track = self.api.get_track(track_id)
+        album = self.api.get_album(track['album']['id'])
+
+        # Get codec and quality info
+        quality_tier_map = {
+            QualityEnum.MINIMUM: 'LOW',
+            QualityEnum.LOW: 'LOW', 
+            QualityEnum.MEDIUM: 'HIGH',
+            QualityEnum.HIGH: 'HIGH',
+            QualityEnum.LOSSLESS: 'LOSSLESS',
+            QualityEnum.HIFI: 'HI_RES'
+        }
+
+        tags = Tags(
+            album_artist=album['artist']['name'],
+            track_number=track['trackNumber'],
+            total_tracks=album['numberOfTracks'],
+            disc_number=track['volumeNumber'],
+            total_discs=album.get('numberOfVolumes', 1),
+            isrc=track.get('isrc'),
+            upc=album.get('upc'),
+            release_date=album.get('releaseDate')
+        )
+
+        return TrackInfo(
+            name=track['title'],
+            album=album['title'],
+            album_id=str(album['id']),
+            artists=[artist['name'] for artist in track.get('artists', [])],
+            artist_id=str(track['artist']['id']),
+            tags=tags,
+            codec=CodecEnum.FLAC,  # Will be updated based on quality
+            cover_url=self._get_cover_url(track['album'].get('cover')),
+            release_year=int(album['releaseDate'].split('-')[0]) if album.get('releaseDate') else None,
+            explicit=track.get('explicit', False),
+            duration=track.get('duration', 0),
+            bit_depth=track.get('audioQuality', {}).get('bitDepth'),
+            sample_rate=track.get('audioQuality', {}).get('sampleRate'),
+            bitrate=track.get('audioQuality', {}).get('bitrate')
+        )
+
+    def get_album_info(self, album_id: str) -> AlbumInfo:
+        album = self.api.get_album(album_id)
+        tracks = album.get('items', [])
+
+        return AlbumInfo(
+            name=album['title'],
+            artist=album['artist']['name'],
+            tracks=[str(track['id']) for track in tracks],
+            release_year=int(album['releaseDate'].split('-')[0]) if album.get('releaseDate') else None,
+            explicit=album.get('explicit', False),
+            artist_id=str(album['artist']['id']),
+            cover_url=self._get_cover_url(album.get('cover')),
+            all_track_cover_jpg_url=self._get_cover_url(album.get('cover'), size=1280),
+            upc=album.get('upc')
+        )
+
+    def get_playlist_info(self, playlist_id: str) -> PlaylistInfo:
+        playlist = self.api.get_playlist(playlist_id)
+        tracks = playlist.get('tracks', {}).get('items', [])
+
+        return PlaylistInfo(
+            name=playlist['title'],
+            creator=playlist.get('creator', {}).get('name', 'Unknown'),
+            tracks=[str(track['id']) for track in tracks],
+            release_year=None,  # Playlists don't have release years
+            explicit=any(track.get('explicit', False) for track in tracks),
+            creator_id=str(playlist.get('creator', {}).get('id')),
+            cover_url=self._get_cover_url(playlist.get('image'))
+        )
+
+    def get_track_download(self, track_id: str, codec: CodecEnum) -> TrackDownloadInfo:
+        download_url = self.api.get_track_url(track_id)
+        return TrackDownloadInfo(
+            download_type=DownloadEnum.URL,
+            file_url=download_url
+        )
+
+    def search(self, query_type: DownloadTypeEnum, query: str, limit: int = 20) -> List[SearchResult]:
+        query_type_map = {
+            DownloadTypeEnum.TRACK: 'tracks',
+            DownloadTypeEnum.ALBUM: 'albums',
+            DownloadTypeEnum.PLAYLIST: 'playlists',
+            DownloadTypeEnum.ARTIST: 'artists'
+        }
+
+        results = self.api.search(query_type_map[query_type], query, limit)
+        items = results.get('items', [])
         
-    async def login(self):
-        """Login to Tidal"""
-        try:
-            await self.api.login()
-        except Exception as e:
-            raise AuthenticationError(f"Failed to login to Tidal: {str(e)}")
-
-    async def get_track(self, url: str) -> Track:
-        """Get track info from URL"""
-        parsed = parse_tidal_url(url)
-        if parsed['type'] != 'track':
-            raise InvalidURLError("URL is not a Tidal track")
+        search_results = []
+        for item in items:
+            if query_type == DownloadTypeEnum.TRACK:
+                result = self._parse_track_search_result(item)
+            elif query_type == DownloadTypeEnum.ALBUM:
+                result = self._parse_album_search_result(item)
+            elif query_type == DownloadTypeEnum.PLAYLIST:
+                result = self._parse_playlist_search_result(item)
+            elif query_type == DownloadTypeEnum.ARTIST:
+                result = self._parse_artist_search_result(item)
+            else:
+                continue
+            
+            search_results.append(result)
         
-        try:
-            track_data = await self.api.get_track(parsed['id'])
-            return Track(
-                id=track_data['id'],
-                title=track_data['title'],
-                artist=track_data['artist']['name'],
-                album=track_data['album']['title'],
-                duration=track_data['duration'],
-                cover_url=self._get_cover_url(track_data['album']['cover']),
-                quality=get_quality_string(self._get_quality_info(track_data))
-            )
-        except Exception as e:
-            raise InvalidURLError(f"Failed to get track info: {str(e)}")
+        return search_results
 
-    async def get_album(self, url: str) -> Album:
-        """Get album info from URL"""
-        parsed = parse_tidal_url(url)
-        if parsed['type'] != 'album':
-            raise InvalidURLError("URL is not a Tidal album")
-        
-        try:
-            album_data = await self.api.get_album(parsed['id'])
-            tracks = []
-            for track in album_data['tracks']['items']:
-                tracks.append(Track(
-                    id=track['id'],
-                    title=track['title'],
-                    artist=track['artist']['name'],
-                    album=album_data['title'],
-                    duration=track['duration'],
-                    cover_url=self._get_cover_url(album_data['cover']),
-                    quality=get_quality_string(self._get_quality_info(track))
-                ))
-            return Album(
-                id=album_data['id'],
-                title=album_data['title'],
-                artist=album_data['artist']['name'],
-                tracks=tracks,
-                cover_url=self._get_cover_url(album_data['cover']),
-                release_date=album_data.get('releaseDate')
-            )
-        except Exception as e:
-            raise InvalidURLError(f"Failed to get album info: {str(e)}")
-
-    async def get_playlist(self, url: str) -> Playlist:
-        """Get playlist info from URL"""
-        parsed = parse_tidal_url(url)
-        if parsed['type'] != 'playlist':
-            raise InvalidURLError("URL is not a Tidal playlist")
-        
-        try:
-            playlist_data = await self.api.get_playlist(parsed['id'])
-            tracks = []
-            for item in playlist_data['tracks']['items']:
-                track = item['item']
-                tracks.append(Track(
-                    id=track['id'],
-                    title=track['title'],
-                    artist=track['artist']['name'],
-                    album=track['album']['title'],
-                    duration=track['duration'],
-                    cover_url=self._get_cover_url(track['album']['cover']),
-                    quality=get_quality_string(self._get_quality_info(track))
-                ))
-            return Playlist(
-                id=playlist_data['uuid'],
-                title=playlist_data['title'],
-                description=playlist_data.get('description'),
-                author=playlist_data['creator']['name'],
-                tracks=tracks,
-                cover_url=self._get_cover_url(playlist_data.get('image'))
-            )
-        except Exception as e:
-            raise InvalidURLError(f"Failed to get playlist info: {str(e)}")
-
-    async def download_track(self, track: Track, quality: str) -> str:
-        """Download track with specified quality"""
-        try:
-            download_url = await self.api.get_track_download_url(track.id, quality)
-            file_path = await self.api.download_file(
-                download_url, 
-                format_track_filename(track)
-            )
-            return file_path
-        except Exception as e:
-            raise DownloadError(f"Failed to download track: {str(e)}")
-
-    async def search(self, query: str, search_type: str = 'track', limit: int = 10) -> List[Dict[str, Any]]:
-        """Search for tracks, albums, or playlists"""
-        try:
-            results = await self.api.search(query, search_type, limit)
-            formatted_results = []
-            for item in results[f'{search_type}s']['items']:
-                if search_type == 'track':
-                    formatted_results.append({
-                        'id': item['id'],
-                        'title': item['title'],
-                        'artist': item['artist']['name'],
-                        'album': item['album']['title'],
-                        'duration': item['duration'],
-                        'quality': get_quality_string(self._get_quality_info(item))
-                    })
-                elif search_type == 'album':
-                    formatted_results.append({
-                        'id': item['id'],
-                        'title': item['title'],
-                        'artist': item['artist']['name'],
-                        'release_date': item.get('releaseDate'),
-                        'tracks': item['numberOfTracks']
-                    })
-                elif search_type == 'playlist':
-                    formatted_results.append({
-                        'id': item['uuid'],
-                        'title': item['title'],
-                        'author': item['creator']['name'],
-                        'tracks': item['numberOfTracks']
-                    })
-            return formatted_results
-        except Exception as e:
-            raise Exception(f"Search failed: {str(e)}")
-
-    def _get_cover_url(self, cover_id: str) -> str:
-        """Generate cover URL from cover ID"""
+    def _get_cover_url(self, cover_id: str, size: int = 1280) -> Optional[str]:
         if not cover_id:
             return None
-        return f"https://resources.tidal.com/images/{cover_id.replace('-', '/')}/1280x1280.jpg"
+        return f"https://resources.tidal.com/images/{cover_id.replace('-', '/')}/{size}x{size}.jpg"
 
-    def _get_quality_info(self, track_data: Dict) -> Dict[str, Any]:
-        """Get available quality info for track"""
-        quality_info = {}
-        if track_data.get('audioQuality') == 'HI_RES':
-            quality_info['hifi'] = True
-            quality_info['mqa'] = True
-        if track_data.get('audioModes'):
-            if 'DOLBY_ATMOS' in track_data['audioModes']:
-                quality_info['dolby_atmos'] = True
-            if 'SONY_360RA' in track_data['audioModes']:
-                quality_info['sony_360'] = True
-        return quality_info
+    def _parse_track_search_result(self, item: dict) -> SearchResult:
+        return SearchResult(
+            result_id=str(item['id']),
+            name=item['title'],
+            artists=[artist['name'] for artist in item.get('artists', [])],
+            year=item.get('releaseDate', '').split('-')[0] if item.get('releaseDate') else None,
+            explicit=item.get('explicit', False),
+            duration=item.get('duration', 0),
+            additional=[item.get('audioQuality', '')]
+        )
+
+    def _parse_album_search_result(self, item: dict) -> SearchResult:
+        return SearchResult(
+            result_id=str(item['id']),
+            name=item['title'],
+            artists=[item['artist']['name']],
+             year=item.get('releaseDate', '').split('-')[0] if item.get('releaseDate') else None,
+            explicit=item.get('explicit', False),
+            additional=[]
+        )
+
+    def _parse_playlist_search_result(self, item: dict) -> SearchResult:
+        return SearchResult(
+            result_id=str(item['id']),
+            name=item['title'],
+            artists=[item['creator']['name']],
+            year=None,
+            explicit=item.get('explicit', False),
+            additional=[]
+        )
+
+    def _parse_artist_search_result(self, item: dict) -> SearchResult:
+        return SearchResult(
+            result_id=str(item['id']),
+            name=item['name'],
+            artists=[],
+            year=None,
+            explicit=False,
+            additional=[]
+        )
